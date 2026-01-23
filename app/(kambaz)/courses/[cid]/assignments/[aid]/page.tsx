f0449d9a16fd931b728893814b632b77b1e6416b
Aidan Roche <roche.ai@husky.neu.edu>
2026-01-22 19:59:30 -0500

    Fix assignment page errors

--- app/(kambaz)/courses/[cid]/assignments/[aid]/page.tsx
+++ app/(kambaz)/courses/[cid]/assignments/[aid]/page.tsx
@@ -11,101 +11,101 @@ export default function AssignmentEditor() {
       />
       <br />
       <table>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-points">Points</label>
-          </td>
-          <td>
-            <input id="wd-points" type="number" defaultValue={100} />
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-group">Assignment Group</label>
-          </td>
-          <td>
-            <select id="wd-group">
-              <option value="ASSIGNMENTS">ASSIGNMENTS</option>
-              <option value="QUIZZES">QUIZZES</option>
-              <option value="EXAMS">EXAMS</option>
-              <option value="PROJECTS">PROJECTS</option>
-            </select>
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-display-grade-as">Display Grade as</label>
-          </td>
-          <td>
-            <select id="wd-display-grade-as">
-              <option value="PERCENTAGE">Percentage</option>
-              <option value="LETTER">Letter</option>
-            </select>
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-submission-type">Submission Type</label>
-          </td>
-          <td>
-            <select id="wd-submission-type">
-              <option value="ONLINE">Online</option>
-              <option value="PAPER">Paper</option>
-            </select>
-            <br></br>
-            <label>Online Entry Options</label>
-            <br />
-            <input
-              type="checkbox"
-              name="check-online-entry-option"
-              id="wd-text-entry"
-            />
-            <label htmlFor="wd-text-entry">Text Entry</label>
-            <br />
-            <input
-              type="checkbox"
-              name="check-online-entry-option"
-              id="wd-website-url"
-            />
-            <label htmlFor="wd-website-url">Website URL</label>
-            <br />
-            <input
-              type="checkbox"
-              name="check-online-entry-option"
-              id="wd-media-recordings"
-            />
-            <label htmlFor="wd-media-recordings">Media Recordings</label>
-            <br />
-            <input
-              type="checkbox"
-              name="check-online-entry-option"
-              id="wd-student-annotation"
-            />
-            <label htmlFor="wd-student-annotation">Student Annotation</label>
-            <br />
-            <input
-              type="checkbox"
-              name="check-online-entry-option"
-              id="wd-file-upload"
-            />
-            <label htmlFor="wd-file-upload">File Uploads</label>
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-assign-to"> Assign </label>
-          </td>
-          <td>
-            <label htmlFor="wd-assign-to">Assign To:</label>
-            <br></br>
-            <input type="text" defaultValue="Everyone" id="wd-assign-to" />
-            <br></br>
+        <tbody>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-points">Points</label>
+            </td>
+            <td>
+              <input id="wd-points" type="number" defaultValue={100} />
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-group">Assignment Group</label>
+            </td>
+            <td>
+              <select id="wd-group">
+                <option value="ASSIGNMENTS">ASSIGNMENTS</option>
+                <option value="QUIZZES">QUIZZES</option>
+                <option value="EXAMS">EXAMS</option>
+                <option value="PROJECTS">PROJECTS</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-display-grade-as">Display Grade as</label>
+            </td>
+            <td>
+              <select id="wd-display-grade-as">
+                <option value="PERCENTAGE">Percentage</option>
+                <option value="LETTER">Letter</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-submission-type">Submission Type</label>
+            </td>
+            <td>
+              <select id="wd-submission-type">
+                <option value="ONLINE">Online</option>
+                <option value="PAPER">Paper</option>
+              </select>
+              <br></br>
+              <label>Online Entry Options</label>
+              <br />
+              <input
+                type="checkbox"
+                name="check-online-entry-option"
+                id="wd-text-entry"
+              />
+              <label htmlFor="wd-text-entry">Text Entry</label>
+              <br />
+              <input
+                type="checkbox"
+                name="check-online-entry-option"
+                id="wd-website-url"
+              />
+              <label htmlFor="wd-website-url">Website URL</label>
+              <br />
+              <input
+                type="checkbox"
+                name="check-online-entry-option"
+                id="wd-media-recordings"
+              />
+              <label htmlFor="wd-media-recordings">Media Recordings</label>
+              <br />
+              <input
+                type="checkbox"
+                name="check-online-entry-option"
+                id="wd-student-annotation"
+              />
+              <label htmlFor="wd-student-annotation">Student Annotation</label>
+              <br />
+              <input
+                type="checkbox"
+                name="check-online-entry-option"
+                id="wd-file-upload"
+              />
+              <label htmlFor="wd-file-upload">File Uploads</label>
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-assign-to"> Assign </label>
+            </td>
+            <td>
+              <label htmlFor="wd-assign-to">Assign To:</label>
+              <br></br>
+              <input type="text" defaultValue="Everyone" id="wd-assign-to" />
+            </td>
             <td>
               <label htmlFor="wd-due-date"> Due: </label>
               <br></br>
               <input type="date" defaultValue="2026-01-25" id="wd-due-date" />
             </td>
-            <br></br>
             <td>
               <label htmlFor="wd-available-from"> Available From: </label>
               <label htmlFor="wd-available-until"> Until: </label>
@@ -121,8 +121,8 @@ export default function AssignmentEditor() {
                 id="wd-available-until"
               />
             </td>
-          </td>
-        </tr>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
